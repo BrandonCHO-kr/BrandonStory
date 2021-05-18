92bc676948b62e98334f3fe77eae5dcb7ea99d42
BrandonCHO-kr <84004515+BrandonCHO-kr@users.noreply.github.com>
2021-05-18 21:00:18 +0900

    Add files via upload
    과제

---
+++ Assignment/Assignment_4.java
@@ -0,0 +1,24 @@
+
+public class Assignment_4 {
+
+	public static void main(String[] args) {
+		int sum,avg;
+		
+		int name1 = 20;
+		int name2 = 50;
+		int name3 = 100;
+		
+		sum = name1 + name2 + name3;
+		avg = (name1+name2+name3)/3;
+		
+		System.out.println("sum = " + sum + "\n" + "avg = " + avg);
+		
+		
+
+				
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Assignment/Assignment_5.java
@@ -0,0 +1,20 @@
+
+public class Assignment_5 {
+
+	public static void main(String[] args) {
+		final double YARD1 = 91.44;
+		
+		double yard = 10.1; 
+		
+		double result = yard * YARD1;
+		
+		
+		System.out.println(yard + "yard = " + (String.format("%.1f",result)) + "cm");
+		
+		
+				
+		// TODO Auto-generated method stub
+
+	}
+
+}
